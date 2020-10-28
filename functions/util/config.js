d39025874ea3927935f115714a13e416b2a0346d
Govind Brahmanyapura <govind.sartaj@gmail.com>
2020-10-28 17:19:55 -0500

    update gitignore to hide db auth stuff

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyB37JG1mOP5y3PNDDCAGal1IEkqLoL44t8",
-    authDomain: "comstore-ga.firebaseapp.com",
-    databaseURL: "https://comstore-ga.firebaseio.com",
-    projectId: "comstore-ga",
-    storageBucket: "comstore-ga.appspot.com",
-    messagingSenderId: "108556902424",
-    appId: "1:108556902424:web:52bd72abd9e0acd3d4a36d",
-  };
